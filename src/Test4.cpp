016a12551dca7058de1b38d0e0ad6ffba270b801
kari <mr.kari.ikonen@gmail.com>
2020-12-27 11:54:13 +0200

    FIX remove gc

--- src/Test4.cpp
+++ src/Test4.cpp
@@ -1,6 +1,7 @@
 #include "Test4.h"
 
 #include <glm/glm.hpp>
+#include <glm/ext.hpp>
 
 Test4::Test4() {
 	title = "Test 4";
@@ -25,21 +26,6 @@ int Test4::onRender(float dt) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glEnable(GL_DEPTH_TEST);
-
-	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-	glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
-	
-	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-	glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
-
-	glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
-
-/*	glm::mat4 view;
-	view = glm::lokAt(glm::vec3(0.0f, 0.0f, 3.0f),
-		glm::vec3(0.0f, 0.0f, 0.0f),
-		glm::vec3(0.0f, 1.0f, 0.0f));
-		*/
 
 	// tri 1
 	mesh->bind(camera, dt);
