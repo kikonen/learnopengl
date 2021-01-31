7ed65d1a5983989460ed863c40a87244cc15f75e
kari <mr.kari.ikonen@gmail.com>
2021-01-31 12:04:32 +0200

    FIX need to init local var

--- shader/test6/tex.fs
+++ shader/test6/tex.fs
@@ -82,10 +82,11 @@ void main() {
 
   vec4 emission = matEmission;
 
+  // https://community.khronos.org/t/default-value-of-uninitialized-local-variable-and-uniforms/74701/2
   bool hasLight = false;
-  vec4 dirShaded;
-  vec4 pointShaded;
-  vec4 spotShaded;
+  vec4 dirShaded = vec4(0);
+  vec4 pointShaded = vec4(0);
+  vec4 spotShaded = vec4(0);
 
   if (light.use) {
     dirShaded = calculateDirLight(light, norm, viewDir, matAmbient, matDiffuse, matSpecular, matShininess, hasNormalMap, fs_in.fragPosLightSpace);
