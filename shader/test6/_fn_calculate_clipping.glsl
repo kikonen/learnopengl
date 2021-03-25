3ca59ac968c34409336494b364fd7afc7bceed00
kari <mr.kari.ikonen@gmail.com>
2021-03-26 01:02:40 +0200

    FIX slightly incorrect clipplane check (xyz + d)

--- shader/test6/_fn_calculate_clipping.glsl
+++ shader/test6/_fn_calculate_clipping.glsl
@@ -1,5 +1,7 @@
 void calculateClipping(vec4 worldPos) {
   for (int i = 0; i < CLIP_COUNT; i++) {
-    gl_ClipDistance[i] = clipping[i].enabled ? dot(worldPos, clipping[i].plane) : 1.0;
+    gl_ClipDistance[i] = clipping[i].enabled
+      ? dot(worldPos.xyz, clipping[i].plane.xyz) + clipping[i].plane.w
+      : 1.0;
   }
 }
