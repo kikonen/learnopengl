d80deea760440b9d47f7220b07f60b96cd5b3232
kari <mr.kari.ikonen@gmail.com>
2021-03-29 00:38:17 +0300

    ADD NOTE for to investigate optimization

--- shader/test6/tex.fs
+++ shader/test6/tex.fs
@@ -8,6 +8,10 @@
 #include uniform_lights.glsl
 #include uniform_materials.glsl
 
+// https://www.khronos.org/opengl/wiki/Early_Fragment_Test
+// https://www.gamedev.net/forums/topic/700517-performance-question-alpha-texture-vs-frag-shader-discard/5397906/
+//layout(early_fragment_tests) in;
+
 in VS_OUT {
   vec3 fragPos;
   vec3 normal;
