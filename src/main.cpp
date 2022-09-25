d5bff6004abe578e79372598d1cd2176aaf2f5da
kari <mr.kari.ikonen@gmail.com>
2022-09-26 01:06:42 +0300

    ADD luajit test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 #include <fstream>
+#include <exception>
 
 #include <entt/entt.hpp>
+
+#include <luajit/lua.hpp>
 
 #include "Engine.h"
 #include "Test6.h"
 
+void testLua() {
+    lua_State* state = luaL_newstate();
 
-int main()
-{
-    entt::registry registry;
-    Log::init();
-    KI_INFO("START");
+    luaL_openlibs(state);
 
+    lua_getglobal(state, "print");
+    lua_pushnumber(state, 4.5);
+
+    if (lua_pcall(state, 1, 1, 0)) {
+        std::cout << "ERROR: " << lua_tostring(state, -1);
+    }
+
+    lua_getglobal(state, "print");
+    lua_pushstring(state, "long string\nanother line\n");
+    if (lua_pcall(state, 1, 1, 0)) {
+        std::cout << "ERROR: " << lua_tostring(state, -1);
+    }
+
+    lua_close(state);
+}
+
+int runEngine() {
     auto engine = std::make_unique<Test6>();
 
     KI_INFO("START: ENGINE INIT");
@@ -23,6 +41,16 @@ int main()
     KI_INFO("DONE: ENGINE INIT");
 
     engine->run();
+}
+
+int main()
+{
+    entt::registry registry;
+    Log::init();
+    KI_INFO("START");
+
+    testLua();
+    runEngine();
 
     KI_INFO("DONE");
 
