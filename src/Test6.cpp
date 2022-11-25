545b17671d57b3622b0ed233a96c37175332922f
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-11-25 22:47:40 +0200

    ADD water debug scene

--- src/Test6.cpp
+++ src/Test6.cpp
@@ -176,9 +176,10 @@ std::shared_ptr<Scene> Test6::loadScene()
 
     m_asyncLoader->m_scene = scene;
 
-    m_file = std::make_unique<SceneFile>(m_asyncLoader.get(), m_assets, "scene/scene_full.yml");
+    //m_file = std::make_unique<SceneFile>(m_asyncLoader.get(), m_assets, "scene/scene_full.yml");
     //m_file = std::make_unique<SceneFile>(m_asyncLoader.get(), m_assets, "scene/scene_player.yml");
     //m_file = std::make_unique<SceneFile>(m_asyncLoader.get(), m_assets, "scene/scene_origo.yml");
+    m_file = std::make_unique<SceneFile>(m_asyncLoader.get(), m_assets, "scene/scene_water.yml");
     m_file->load(scene);
 
     m_testSetup = std::make_unique<TestSceneSetup>(m_asyncLoader.get(), m_assets);
