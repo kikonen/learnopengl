457aa3af441ad0bd5f43c37bc1741512f1e3d27a
kari <mr.kari.ikonen@gmail.com>
2023-01-01 12:36:30 +0200

    FIX missing files

---
+++ shader/test6/_uniform_instance_object_id.glsl
@@ -0,0 +1,3 @@
+layout (std430, binding = SSBO_INSTANCE_OBJECT_ID) readonly buffer ObjectID {
+  uint u_objectIDs[];
+};
---
+++ shader/test6/_uniform_material_indeces.glsl
@@ -0,0 +1,3 @@
+layout (std430, binding = SSBO_MATERIAL_INDECES) readonly buffer MaterialIndeces {
+  float u_materialIndeces[];
+};
