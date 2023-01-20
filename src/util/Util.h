44abeb28c408f329e801a6f12068fef28cabd200
kari <mr.kari.ikonen@gmail.com>
2023-01-20 22:54:43 +0200

    FIX fmt for class enum

--- src/util/Util.h
+++ src/util/Util.h
@@ -8,4 +8,11 @@ namespace util
 
     const std::string& toLower(std::string& str);
 
+    // https://stackoverflow.com/questions/11421432/how-can-i-output-the-value-of-an-enum-class-in-c11
+    template <typename Enumeration>
+    auto as_integer(Enumeration const value)
+        -> typename std::underlying_type<Enumeration>::type
+    {
+        return static_cast<typename std::underlying_type<Enumeration>::type>(value);
+    }
 }
