755fe5d087929aeb3d514116ed41790483a60fa3
Kari Ikonen <mr.kari.ikonen@gmail.com>
2023-01-22 16:55:27 +0200

    FIX validation and avoiding memory leak

--- src/scene/CubeMap.cpp
+++ src/scene/CubeMap.cpp
@@ -52,17 +52,30 @@ unsigned int CubeMap::createFromImages(
 
     glCreateTextures(GL_TEXTURE_CUBE_MAP, 1, &textureID);
 
-    int w = 0, h = 0;
+    int w = -1;
+    int h = -1;
 
-    Image* images[6];
+    std::vector<std::unique_ptr<Image>> images{ 6 };
 
     for (unsigned int i = 0; i < 6; i++)
     {
-        Image* image = new Image(faces[i]);
-        images[i] = image;
+        images[i] = std::make_unique<Image>(faces[i]);
+        Image* image = images[i].get();
+
         if (image->load(false)) continue;
-        w = image->m_width;
-        h = image->m_height;
+
+        if (w == -1) {
+            w = image->m_width;
+            h = image->m_height;
+        }
+        else {
+            if (w != image->m_width || h != image->m_height) {
+                throw std::runtime_error{
+                    fmt::format(
+                        "cubemap texture size mismatch {}=({}, {}) vs. {}=({}, {})",
+                        faces[0], w, h, faces[i], image->m_width, image->m_height) };
+            }
+        }
     }
 
     glTextureStorage2D(textureID, 1, internalFormat, w, h);
@@ -70,14 +83,10 @@ unsigned int CubeMap::createFromImages(
     // https://github.com/fendevel/Guide-to-Modern-OpenGL-Functions
     for (unsigned int i = 0; i < 6; i++)
     {
-        Image* image = images[i];
-        if (!image->m_data) continue;
+        Image* image = images[i].get();
+        if (!image || !image->m_data) continue;
 
         glTextureSubImage3D(textureID, 0, 0, 0, i, image->m_width, image->m_height, 1, format, GL_UNSIGNED_BYTE, image->m_data);
-    }
-
-    for (unsigned int i = 0; i < 6; i++) {
-        delete images[i];
     }
 
     glTextureParameteri(textureID, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
