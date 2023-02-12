7f004569fbedd439417c9c116e8ceffd7b7484f0
Kari Ikonen <mr.kari.ikonen@gmail.com>
2023-02-12 15:10:44 +0200

    FIX cleanup

--- src/asset/Texture.h
+++ src/asset/Texture.h
@@ -3,20 +3,9 @@
 #include <string>
 
 #include "asset/Assets.h"
+#include "TextureSpec.h"
 
 #include "ki/GL.h"
-
-constexpr int MIP_MAP_LEVELS = 5;
-
-struct TextureSpec {
-    GLint wrapS = GL_CLAMP_TO_EDGE;
-    GLint wrapT = GL_CLAMP_TO_EDGE;
-
-    int minFilter = GL_NEAREST_MIPMAP_LINEAR;
-    int magFilter = GL_LINEAR;
-
-    int mipMapLevels = MIP_MAP_LEVELS;
-};
 
 /*
 * https://learnopengl.com/Getting-started/Textures
---
+++ src/asset/TextureSpec.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ki/GL.h"
+
+constexpr int MIP_MAP_LEVELS = 8;
+
+struct TextureSpec {
+    GLint wrapS = GL_CLAMP_TO_EDGE;
+    GLint wrapT = GL_CLAMP_TO_EDGE;
+
+    int minFilter = GL_NEAREST_MIPMAP_LINEAR;
+    int magFilter = GL_LINEAR;
+
+    int mipMapLevels = MIP_MAP_LEVELS;
+};
