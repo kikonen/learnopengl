5f76aca2f35ebeb25e63c37a623e0293caf58e74
kari <mr.kari.ikonen@gmail.com>
2023-02-14 23:02:11 +0200

    FIX water map clip

--- src/renderer/WaterMapRenderer.cpp
+++ src/renderer/WaterMapRenderer.cpp
@@ -153,7 +153,7 @@ bool WaterMapRenderer::render(
 
         ClipPlaneUBO& clip = localCtx.m_clipPlanes.clipping[0];
         clip.enabled = true;
-        clip.plane = glm::vec4(0, (sdist > 0 ? 1 : -1), 0, (sdist > 0 ? -1 : 1) * planePos.y);
+        clip.plane = glm::vec4(0, (sdist > 0 ? 1 : 1), 0, (sdist > 0 ? -1 : 1) * planePos.y);
 
         localCtx.updateMatricesUBO();
 
@@ -188,7 +188,7 @@ bool WaterMapRenderer::render(
         // TODO KI refraction plane clipping produces inproper clipping
         ClipPlaneUBO& clip = localCtx.m_clipPlanes.clipping[0];
         clip.enabled = true;
-        clip.plane = glm::vec4(0, (sdist > 0 ? -0.99 : 0.99), 0, (sdist > 0 ? 0.99 : -0.99) * planePos.y);
+        clip.plane = glm::vec4(0, (sdist > 0 ? 1 : 1), 0, (sdist > 0 ? 1 : -1) * planePos.y);
 
         localCtx.updateMatricesUBO();
 
