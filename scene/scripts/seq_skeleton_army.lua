8915fbeccf9d6fc17195cea06290fd3cbb536351
kari <mr.kari.ikonen@gmail.com>
2023-02-19 12:08:11 +0200

    ADD constant skeleton army attack

--- scene/scripts/seq_skeleton_army.lua
+++ scene/scripts/seq_skeleton_army.lua
@@ -1,4 +1,6 @@
-local function main()
+local luaNode = nodes[id]
+
+local function attack(wid)
   --print(string.format("START: %d", id))
 
   local pos = node:getPos()
@@ -6,10 +8,8 @@ local function main()
   local x = pos[1]
   local y = pos[2]
   local z = pos[3]
-  local wid = -1
   local cid = -1
 
-  wid = cmd:wait(0, 5)
   cid = cmd:move(id, { after=wid, time=10, relative=true }, { 25 - rnd(100), 0, 25 - rnd(100) })
 
   wid = cmd:wait(0, 7)
@@ -22,7 +22,32 @@ local function main()
   cid = cmd:move(id, { after=cid, time=5, relative=true }, { 10 - rnd(20), 0, 10 - rnd(20) })
   cid = cmd:moveSpline(id, { after=cid, time=3, relative=true }, { 20, 0, 5 }, { 5 - rnd(10), 0, 5 - rnd(10) })
   cid = cmd:move(id, { after=cid, time=2, relative=false }, { x, y, z })
+
+  return cid;
 end
 
-cmd:start(id, { after=wid }, main)
---main()
+local function animation()
+   local wid = -1
+   local cid = -1
+
+   local origPos = node:getPos()
+
+   while(true) do
+      -- NOTE KI *WAIT* for resume to complete
+      wid = cmd:wait(cid, 1)
+
+      cid = attack(wid)
+
+      wid = cmd:wait(cid, 10)
+      cid = cmd:resume(id, { after=wid }, "callback")
+
+      -- NOTE KI wait for callback
+      coroutine.yield()
+   end
+end
+
+luaNode.start = function()
+   print(string.format("START: name=%s, id=%d, clone=%d", node:getName(), id, node:getCloneIndex()))
+   luaNode.callback = coroutine.wrap(animation)
+   cmd:resume(id, {}, "callback")
+end
