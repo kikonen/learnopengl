146d38750a0fc75c1369f9c3efd8c8f0caea4ad2
kari <mr.kari.ikonen@gmail.com>
2023-03-06 22:11:07 +0200

    FIX comment out debug code

--- src/command/api/MoveSplineNode.cpp
+++ src/command/api/MoveSplineNode.cpp
@@ -27,11 +27,13 @@ void MoveSplineNode::bind(const RenderContext& ctx, Node* node) noexcept
         const auto& nodePosition = m_node->getPosition();
         m_end -= nodePosition;
 
-        if (m_node->m_id == KI_UUID("65ce67c8-3efe-4b04-aaf9-fe384152c547"))
-            KI_INFO_OUT(fmt::format(
-                "BIND: pos=({}, {}, {}), target=({}, {}, {}), relative={}",
-                nodePosition.x, nodePosition.y, nodePosition.z,
-                m_end.x, m_end.y, m_end.z, m_relative));
+        //if (m_node->m_id == KI_UUID("65ce67c8-3efe-4b04-aaf9-fe384152c547")) {
+        //    KI_INFO_OUT(fmt::format(
+        //        "BIND-{}-{}-{}: pos=({}, {}, {}), target=({}, {}, {}), relative={}",
+        //        m_id, m_node->m_objectID, KI_UUID_STR(m_node->m_id),
+        //        nodePosition.x, nodePosition.y, nodePosition.z,
+        //        m_end.x, m_end.y, m_end.z, m_relative));
+        //}
     }
 }
 
@@ -42,8 +44,12 @@ void MoveSplineNode::execute(
     m_finished = m_elapsedTime >= m_finishTime;
 
     const auto& nodePosition = m_node->getPosition();
-    if (m_node->m_id == KI_UUID("65ce67c8-3efe-4b04-aaf9-fe384152c547"))
-        KI_INFO_OUT(fmt::format("MOVE: {}, {}, {}", nodePosition.x, nodePosition.y, nodePosition.z));
+    //if (m_node->m_id == KI_UUID("65ce67c8-3efe-4b04-aaf9-fe384152c547")) {
+    //    KI_INFO_OUT(fmt::format(
+    //        "MOVE-{}: {}, {}, {}",
+    //        m_id,
+    //        nodePosition.x, nodePosition.y, nodePosition.z));
+    //}
 
     // NOTE KI keep steps relative to previous
     // => in case there is N concurrent commands
@@ -54,7 +60,7 @@ void MoveSplineNode::execute(
     else {
         const auto t = (m_elapsedTime / m_finishTime);
 
-        glm::vec3 p0{ 0 };
+        glm::vec3 p0{ 0.f };
         glm::vec3 p1{ m_controlPoint };
         glm::vec3 p2{ m_end };
 
@@ -65,12 +71,14 @@ void MoveSplineNode::execute(
     m_node->setPosition(nodePosition + adjust);
     m_previous = position;
 
-    if (m_finished) {
-        if (m_node->m_id == KI_UUID("65ce67c8-3efe-4b04-aaf9-fe384152c547"))
-            const auto& nodePosition = m_node->getPosition();
-            KI_INFO_OUT(fmt::format(
-                "BIND: pos=({}, {}, {}), target=({}, {}, {}), relative={}",
-                nodePosition.x, nodePosition.y, nodePosition.z,
-                m_end.x, m_end.y, m_end.z, m_relative));
-    }
+    //if (m_finished) {
+    //    if (m_node->m_id == KI_UUID("65ce67c8-3efe-4b04-aaf9-fe384152c547")) {
+    //        const auto& nodePosition = m_node->getPosition();
+    //        KI_INFO_OUT(fmt::format(
+    //            "FINISH-{}-{}-{}: pos=({}, {}, {}), target=({}, {}, {}), relative={}",
+    //            m_id, m_node->m_objectID, KI_UUID_STR(m_node->m_id),
+    //            nodePosition.x, nodePosition.y, nodePosition.z,
+    //            m_end.x, m_end.y, m_end.z, m_relative));
+    //    }
+    //}
 }
