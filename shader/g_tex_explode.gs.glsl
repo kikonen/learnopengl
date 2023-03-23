5c457360f9cc7c50c06d506e01273ad65cd0ed75
kari <mr.kari.ikonen@gmail.com>
2023-03-23 23:32:42 +0200

    ADD g_tex_explode.gs.glsl

---
+++ shader/g_tex_explode.gs.glsl
@@ -0,0 +1,89 @@
+#version 460 core
+
+layout (triangles) in;
+layout (triangle_strip, max_vertices = 3) out;
+
+#include struct_clip_plane.glsl
+
+#include uniform_data.glsl
+#include uniform_clip_planes.glsl
+
+in VS_OUT {
+  vec3 worldPos;
+  vec3 normal;
+  vec2 texCoord;
+  vec3 vertexPos;
+
+  flat uint materialIndex;
+
+#ifdef USE_NORMAL_TEX
+  flat mat3 TBN;
+#endif
+} vs_in[];
+
+out VS_OUT {
+  vec3 worldPos;
+  vec3 normal;
+  vec2 texCoord;
+  vec3 vertexPos;
+
+  flat uint materialIndex;
+
+#ifdef USE_NORMAL_TEX
+  flat mat3 TBN;
+#endif
+} gs_out;
+
+//out float gl_ClipDistance[CLIP_COUNT];
+
+////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////
+
+precision mediump float;
+
+//#include fn_calculate_clipping.glsl
+
+vec3 getNormal()
+{
+    vec3 a = vec3(gl_in[0].gl_Position) - vec3(gl_in[1].gl_Position);
+    vec3 b = vec3(gl_in[2].gl_Position) - vec3(gl_in[1].gl_Position);
+    return normalize(cross(a, b));
+}
+
+vec4 explode(in vec4 pos, in vec3 normal)
+{
+  float magnitude = 2.0;
+  float t = u_time;
+  vec3 direction = normal * ((sin(t) + 1.0) / 2.0) * magnitude;
+  return pos + vec4(direction, 0.0);
+}
+
+void sendVertex(in int i, in vec4 pos) {
+  gl_Position = pos;
+
+  gs_out.worldPos = vs_in[i].worldPos;
+  gs_out.normal = vs_in[i].normal;
+  gs_out.texCoord = vs_in[i].texCoord;
+  gs_out.vertexPos = vs_in[i].vertexPos;
+
+  gs_out.materialIndex = vs_in[i].materialIndex;
+
+  gs_out.TBN = vs_in[i].TBN;
+
+//  calculateClipping(vec4(gs_out.worldPos, 1.0));
+
+  EmitVertex();
+}
+
+////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////
+
+void main() {
+  vec3 normal = getNormal();
+  for (int i = 0; i < vs_in.length(); i++) {
+    sendVertex(i, explode(gl_in[i].gl_Position, normal));
+  }
+  EndPrimitive();
+}
