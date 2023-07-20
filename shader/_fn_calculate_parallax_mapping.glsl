e8981b91c657088dafb0dee83f2dfbdd3633900a
kari <mr.kari.ikonen@gmail.com>
2023-07-20 22:16:42 +0300

    FIX try own displacement bricks; not looking good

--- shader/_fn_calculate_parallax_mapping.glsl
+++ shader/_fn_calculate_parallax_mapping.glsl
@@ -5,7 +5,7 @@ vec2 calculateParallaxMapping(
   vec3 toView)
 {
   sampler2D sampler = sampler2D(u_texture_handles[material.heightMapTex]);
-  float height =  texture(sampler, texCoord).r;
+  float height = texture(sampler, texCoord).r;
   vec2 p = toView.xy / toView.z * (height * material.depth);
   return texCoord - p;
 }
