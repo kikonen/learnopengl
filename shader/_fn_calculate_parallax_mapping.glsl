356d35a6bf5b5225e50abec9272dd110e53e336d
kari <mr.kari.ikonen@gmail.com>
2023-07-22 23:47:31 +0300

    WIP parallax occlusion mapping

--- shader/_fn_calculate_parallax_mapping.glsl
+++ shader/_fn_calculate_parallax_mapping.glsl
@@ -46,4 +46,53 @@ vec2 calculateDeepParallaxMapping(
 
   return currentTexCoord;
 }
+
+vec2 calculateParallaxOcclusionMapping(
+  in Material material,
+  vec2 texCoord,
+  vec3 toView)
+{
+  sampler2D sampler = sampler2D(u_texture_handles[material.heightMapTex]);
+
+  // number of depth layers
+  const float minLayers = 8.0;
+  const float maxLayers = 32.0;
+  const float numLayers = mix(maxLayers, minLayers, max(dot(vec3(0.0, 0.0, 1.0), toView), 0.0));
+
+  // calculate the size of each layer
+  float layerDepth = 1.0 / numLayers;
+  // depth of current layer
+  float currentLayerDepth = 0.0;
+  // the amount to shift the texture coordinates per layer (from vector P)
+  vec2 P = toView.xy * material.depth;
+  vec2 deltaTexCoord = P / numLayers;
+
+  // get initial values
+  vec2  currentTexCoord     = texCoord;
+  float currentDepthMapValue = texture(sampler, currentTexCoord).r;
+
+  while(currentLayerDepth < currentDepthMapValue)
+  {
+    // shift texture coordinates along direction of P
+    currentTexCoord -= deltaTexCoord;
+    // get depthmap value at current texture coordinates
+    currentDepthMapValue = texture(sampler, currentTexCoord).r;
+    // get depth of next layer
+    currentLayerDepth += layerDepth;
+  }
+
+// get texture coordinates before collision (reverse operations)
+  vec2 prevTexCoord = currentTexCoord + deltaTexCoord;
+
+// get depth after and before collision for linear interpolation
+  float afterDepth  = currentDepthMapValue - currentLayerDepth;
+  float beforeDepth = texture(sampler, prevTexCoord).r - currentLayerDepth + layerDepth;
+
+// interpolation of texture coordinates
+  float weight = afterDepth / (afterDepth - beforeDepth);
+  vec2 finalTexCoord = prevTexCoord * weight + currentTexCoord * (1.0 - weight);
+
+  return finalTexCoord;
+}
+
 #endif
