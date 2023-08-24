5f3c453997a9d28fedec2a3158e17a49b232076f
Kari Ikonen <mr.kari.ikonen@gmail.com>
2023-08-24 23:17:53 +0300

    FIX redundant clamp

--- shader/_fn_calculate_dir_light.glsl
+++ shader/_fn_calculate_dir_light.glsl
@@ -39,7 +39,6 @@ vec3 calculateDirLight(
   // calculate shadow
   //float shadow = calcShadow2_3(worldPos, shadowIndex, shadowPos, normal, lightDir);
   float shadow = calcShadow2_5(u_shadowMap[shadowIndex], shadowPos);
-  shadow = clamp(shadow, 0.2, 1.0);
 
   return shadow * (diffuse + specular);
 }
