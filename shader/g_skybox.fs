bb2ef930e9fa1197fba4e4542bde21e06446d781
kari <mr.kari.ikonen@gmail.com>
2023-09-26 14:45:15 +0300

    FIX remove redundant tex access

--- shader/g_skybox.fs
+++ shader/g_skybox.fs
@@ -6,8 +6,8 @@ layout(early_fragment_tests) in;
 #include uniform_data.glsl
 
 layout(binding = UNIT_SKYBOX) uniform samplerCube u_skybox;
-layout(binding = UNIT_IRRADIANCE_MAP) uniform samplerCube u_irradianceMap;
-layout(binding = UNIT_PREFILTER_MAP) uniform samplerCube u_prefilterMap;
+//layout(binding = UNIT_IRRADIANCE_MAP) uniform samplerCube u_irradianceMap;
+//layout(binding = UNIT_PREFILTER_MAP) uniform samplerCube u_prefilterMap;
 
 LAYOUT_G_BUFFER_OUT;
 
@@ -21,15 +21,15 @@ SET_FLOAT_PRECISION;
 
 void main() {
   vec3 envColor = texture(u_skybox, texCoord).rgb;
-  vec3 irradianceColor = texture(u_irradianceMap, texCoord).rgb;
-  vec3 prefilterColor = textureLod(u_prefilterMap, texCoord, 0).rgb;
+//  vec3 irradianceColor = texture(u_irradianceMap, texCoord).rgb;
+//  vec3 prefilterColor = textureLod(u_prefilterMap, texCoord, 0).rgb;
 
   //texColor = vec4(1.0, 0, 0, 1.0);
   //texColor.a = 0.0;
 
   vec3 texColor = envColor;
-  texColor = mix(texColor, irradianceColor, 0.0);
-  texColor = mix(texColor, prefilterColor, 0.0);
+//  texColor = mix(texColor, irradianceColor, 0.0);
+//  texColor = mix(texColor, prefilterColor, 0.0);
 
   o_fragColor = vec4(texColor.rgb, 1.0);
   //o_fragSpecular = vec4(0);
