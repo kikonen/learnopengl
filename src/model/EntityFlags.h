39fdc5025124cb0fa5e72a9b5c7442a353b984de
Kari Ikonen <mr.kari.ikonen@gmail.com>
2024-02-12 23:25:11 +0200

    FIX remove redundant flags

--- src/model/EntityFlags.h
+++ src/model/EntityFlags.h
@@ -1,8 +1,6 @@
 #pragma once
 
-constexpr unsigned int ENTITY_DRAW_ELEMENT_BIT = 1;
-constexpr unsigned int ENTITY_DRAW_ARRAY_BIT = 2;
-constexpr unsigned int ENTITY_BILLBOARD_BIT = 4;
-constexpr unsigned int ENTITY_NO_FRUSTUM_BIT = 8;
-constexpr unsigned int ENTITY_SKYBOX_BIT = 16;
-constexpr unsigned int ENTITY_SPRITE_BIT = 32;
+constexpr unsigned int ENTITY_BILLBOARD_BIT = 1;
+constexpr unsigned int ENTITY_SPRITE_BIT = 2;
+constexpr unsigned int ENTITY_NO_FRUSTUM_BIT = 4;
+constexpr unsigned int ENTITY_SKYBOX_BIT = 8;
