83c16105b777857c1b9804510324fb86ccf84b9d
Kari Ikonen <mr.kari.ikonen@gmail.com>
2024-06-07 22:58:16 +0300

    FIX invalid & duplicate bone bindings

--- src/animation/VertexBone.h
+++ src/animation/VertexBone.h
@@ -11,7 +11,13 @@ namespace animation {
         // Insert bone data into first free slot
         // - free == weight not yet set
         void addBone(uint16_t boneId, float weight) {
+            if (weight == 0.f)
+                return;
+
             for (int i = 0; i < 4; i++) {
+                if (m_boneIds[i] == boneId)
+                    return;
+
                 if (m_weights[i] == 0.f) {
                     m_boneIds[i] = boneId;
                     m_weights[i] = weight;
