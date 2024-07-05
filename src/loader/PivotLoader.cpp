042d324f9d9eb9a56b9b84b56b9fc5fd690af1e9
Kari Ikonen <mr.kari.ikonen@gmail.com>
2024-07-06 00:19:53 +0300

    WIP pivot point resolving & apply

--- src/loader/PivotLoader.cpp
+++ src/loader/PivotLoader.cpp
@@ -7,7 +7,7 @@
 #include "loader_util.h"
 
 namespace {
-    const std::string PIVOT_ZERO{ "0" };
+    const std::string PIVOT_ORIGIN{ "o" };
     const std::string PIVOT_MIDDLE{ "M" };
     const std::string PIVOT_TOP{ "T" };
     const std::string PIVOT_BOTTOM{ "B" };
@@ -18,7 +18,7 @@ namespace {
 
     std::map<std::string, PivotAlignment>& getPivotMapping() {
         if (m_pivots.empty()) {
-            m_pivots[PIVOT_ZERO] = PivotAlignment::zero;
+            m_pivots[PIVOT_ORIGIN] = PivotAlignment::origin;
             m_pivots[PIVOT_MIDDLE] = PivotAlignment::middle;
             m_pivots[PIVOT_TOP] = PivotAlignment::top;
             m_pivots[PIVOT_BOTTOM] = PivotAlignment::bottom;
@@ -32,15 +32,13 @@ namespace {
         const auto& mapping = getPivotMapping();
         const auto& it = mapping.find(p);
         if (it != mapping.end()) return it->second;
-        return PivotAlignment::zero;
+        return PivotAlignment::origin;
     }
-}
 
-namespace loader {
-    PivotPoint PivotLoader::read(const loader::DocNode& node) const
+    void readAlignment(
+        const loader::DocNode& node,
+        PivotPoint& pivot)
     {
-        PivotPoint pivot;
-
         auto vec = readStringVector(node, 3);
 
         for (auto& p : vec) {
@@ -53,14 +51,32 @@ namespace loader {
         }
         else {
             while (vec.size() < 3) {
-                vec.push_back(PIVOT_ZERO);
+                vec.push_back(PIVOT_ORIGIN);
             }
         }
 
         for (int i = 0; i < 3; i++) {
-            pivot.alignment[i] = resolvePivot(vec[i]);
+            pivot.m_alignment[i] = resolvePivot(vec[i]);
         }
+    }
+}
 
+namespace loader {
+    PivotPoint PivotLoader::load(const loader::DocNode& node) const
+    {
+        PivotPoint pivot;
+
+        for (const auto& pair : node.getNodes()) {
+            const std::string& k = pair.getName();
+            const loader::DocNode& v = pair.getNode();
+
+            if (k == "alignment") {
+                readAlignment(v, pivot);
+            }
+            else if (k == "offset") {
+                pivot.m_offset = readVec3(v);
+            }
+        }
         return pivot;
     }
 }
--- src/model/PivotPoint.h
+++ src/model/PivotPoint.h
@@ -5,7 +5,7 @@
 #include <glm/glm.hpp>
 
 enum class PivotAlignment : uint8_t {
-    zero,
+    origin,
     middle,
     top,
     bottom,
@@ -18,10 +18,12 @@ namespace mesh {
 }
 
 struct PivotPoint {
-    PivotAlignment alignment[3]{
-        PivotAlignment::zero,
-        PivotAlignment::zero,
-        PivotAlignment::zero
+    glm::vec3 m_offset{ 0.f };
+
+    PivotAlignment m_alignment[3]{
+        PivotAlignment::origin,
+        PivotAlignment::origin,
+        PivotAlignment::origin
     };
 
     glm::vec3 resolve(mesh::MeshType* type) const;
