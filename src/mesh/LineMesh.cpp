20958f2f8adb7872e00ea2819ee3b81c78bf51cd
Kari Ikonen <mr.kari.ikonen@gmail.com>
2024-07-14 21:18:30 +0300

    FIX #19 added LineMesh

---
+++ src/mesh/LineMesh.cpp
@@ -0,0 +1,6 @@
+#include "LineMesh.h"
+
+namespace mesh
+{
+
+}
---
+++ src/mesh/LineMesh.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <string>
+
+#include "mesh/Mesh.h"
+
+#include "mesh/Index.h"
+
+#include "mesh/Vertex.h"
+
+namespace mesh {
+    class LineMesh final : public Mesh
+    {
+    public:
+        LineMesh();
+        virtual ~LineMesh();
+
+        virtual std::string str() const noexcept override;
+
+        void clear();
+
+        virtual AABB calculateAABB(const glm::mat4& transform) const noexcept override;
+
+        virtual const kigl::GLVertexArray* prepareRT(
+            const PrepareContext& ctx) override;
+
+        virtual void prepareLodMesh(
+            mesh::LodMesh& lodMesh) override;
+
+        uint32_t getBaseVertex() const noexcept {
+            return m_vboIndex;
+        }
+
+        inline uint32_t getBaseIndex() const noexcept {
+            return m_eboIndex * 3;
+        }
+
+        inline uint32_t getIndexCount() const noexcept {
+            return static_cast<uint32_t>(m_indeces.size() * 3);
+        }
+
+    public:
+        std::vector<Vertex> m_vertices;
+        std::vector<mesh::Index> m_indeces;
+
+        std::vector<glm::vec2> m_atlasCoords;
+
+        uint32_t m_maxSize{ 0 };
+
+        // NOTE KI absolute index into VBO
+        uint32_t m_vboIndex{ 0 };
+
+        // NOTE KI absolute index into EBO
+        uint32_t m_eboIndex{ 0 };
+    };
+}
