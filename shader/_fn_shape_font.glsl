cc1f8d2139cd4e7633f47d9290d5b11fc60be651
Kari Ikonen <mr.kari.ikonen@gmail.com>
2025-01-16 00:34:38 +0200

    FIX support glow, outline & glow+outline

--- shader/_fn_shape_font.glsl
+++ shader/_fn_shape_font.glsl
@@ -6,19 +6,36 @@ void shapeFont(
   in uvec2 atlasHandle,
   in vec2 atlasCoord,
   in bool useBlend,
-  out vec4 outColor)
+  out vec4 result)
 {
+#ifdef USE_OUTLINE
+#ifdef USE_GLOW
+
+#undef USE_OUTLINE
+#undef USE_GLOW
+#define USE_GLOW_OUTLINE
+
+#endif
+#endif
+
   sampler2D atlas = sampler2D(atlasHandle);
 
   const float glyphCenter = 0.5;
+  const vec3 glyphColor = material.diffuse.rgb;
+
+// #ifdef USE_OUTLINE
   const float outlineCenter = 0.55;
+  // const vec3 outlineColor  = vec3(0.0, 0.0, 1.0);
+  const vec3 outlineColor  = glyphColor * 0.5;
+// #endif
+
+// #ifdef USE_GLOW
   const float glowCenter = 1.05;
   const vec2 glowOffset = vec2(0, 0);
   // const vec2 glowOffset = vec2(0.003 * cos(u_time), 0.003 * sin(u_time));
-
-  const vec3 fontColor = material.diffuse.rgb;
-  const vec3 outlineColor  = vec3(0.0, 0.0, 1.0);
-  const vec3 glowColor  = vec3(0.0, 1.0, 0.0);
+  // const vec3 glowColor  = vec3(0.0, 1.0, 0.0);
+  const vec3 glowColor = glyphColor * 1.3;
+// #endif
 
   const float dist  = textureLod(atlas, atlasCoord, 0).r;
   const float width = fwidth(dist);
@@ -28,30 +45,34 @@ void shapeFont(
     glyphCenter + width,
     dist);
 
-  vec3 rgb;
+  result = vec4(glyphColor, alpha);
 
+#ifdef USE_OUTLINE
   if (useBlend) {
-    const float beta = smoothstep(
-      outlineCenter - width,
-      outlineCenter + width,
-      dist);
-
-    const float glowDist  = textureLod(atlas, atlasCoord + glowOffset, 0).r;
-    const float glowWidth = fwidth(glowDist);
-    const float glowAlpha = smoothstep(
-      glowCenter - glowWidth,
-      glowCenter + glowWidth,
-      glowDist);
-
-    rgb = mix(glowColor, fontColor, alpha);
-    float mu = smoothstep(glyphCenter, glowCenter, sqrt(glowDist));
-    outColor.rgb = rgb;
-    outColor.a = max(alpha, mu);
-  } else {
-    rgb = fontColor;
-    outColor.rgb = rgb;
-    outColor.a = alpha;
+    float mu = smoothstep(outlineCenter - width, outlineCenter + width, dist);
+    vec3 rgb = mix(outlineColor, glyphColor, mu);
+    result = vec4(rgb, max(alpha, mu));
   }
+#endif
+
+#ifdef USE_GLOW
+  if (useBlend) {
+    vec3 rgb = mix(glowColor, glyphColor, alpha);
+    float mu = smoothstep(glyphCenter, glowCenter, sqrt(dist));
+    result = vec4(rgb, max(alpha, mu));
+  }
+#endif
+
+#ifdef USE_GLOW_OUTLINE
+  if (useBlend) {
+    vec3 rgb = mix(glowColor, glyphColor, alpha);
+    float mu = smoothstep(glyphCenter, glowCenter, sqrt(dist));
+    vec4 color = vec4(rgb, max(alpha, mu));
+    float beta = smoothstep(outlineCenter - width, outlineCenter + width, dist);
+    rgb = mix(outlineColor, color.rgb, beta);
+    result = vec4(rgb, max(color.a, beta));
+  }
+#endif
 
   // if (glowCenter > 0) {
   //   vec3 glowColor  = vec3(0.0, 1.0, 0.0);
@@ -61,16 +82,16 @@ void shapeFont(
 
   //   float glowAlpha = 1.0 - smoothstep(glowCenter - glowWidth, glowCenter - glowWidth, glowDist);
 
-  //   outColor.a = glyphAlpha + (1.0 - alpha) * alpha;
-  //   outColor.rgb = mix(color, color, alpha / color.a);
+  //   result.a = glyphAlpha + (1.0 - alpha) * alpha;
+  //   result.rgb = mix(color, color, alpha / color.a);
   // } else {
-  //   outColor.a = alpha;
-  //   outColor.rgb = color;
+  //   result.a = alpha;
+  //   result.rgb = color;
   // }
 
-  outColor.a *= material.diffuse.a;
+  result.a *= material.diffuse.a;
 
   if (dist >= 0.9) {
-    // outColor = vec4(1, 0, 0, 1);
+    // result = vec4(1, 0, 0, 1);
   }
 }
