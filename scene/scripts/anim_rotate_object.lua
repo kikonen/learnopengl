110d6f2350f0aaf22424ee64b96bef56a8f3a5f4
Kari Ikonen <mr.kari.ikonen@gmail.com>
2025-03-03 22:55:36 +0200

    FIX blend scene broken

--- scene/scripts/anim_rotate_object.lua
+++ scene/scripts/anim_rotate_object.lua
@@ -1,7 +1,6 @@
 --printf("START: name=%s, id=%d, clone=%d\n", node:get_name(), id, node:get_clone_index())
 
 local function animation()
-  local listener_id = nil
   local orig_pos = node:get_pos()
   local wid = 0
   local cid = 0
@@ -15,20 +14,18 @@ local function animation()
       { 0, 1, 0 },
       dir * 45)
 
-    cid = cmd:emit(
-      { after=wid },
-      { type=Event.SCRIPT_RESUME, listener=listener_id})
+    cid = cmd:invoke(
+      { after=cid },
+      animation_listener)
 
     dir = -dir
   end
 
-  listener_id = events:listen(animation_listener, {Event.SCRIPT_RESUME})
-
   wid = cmd:wait({ after=cid, time=1 })
 
-  cid = cmd:emit(
+  cid = cmd:invoke(
     { after=wid },
-    { type=Event.SCRIPT_RESUME, listener=listener_id})
+    animation_listener)
 end
 
 animation()
