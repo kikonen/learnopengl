0c0d2f20110e79f25285b7696000eb1fa2ae29fe
Kari Ikonen <mr.kari.ikonen@gmail.com>
2025-07-20 19:06:45 +0300

    FIX hello text scroller

---
+++ scene/scripts/scroll_hello_text.lua
@@ -0,0 +1,28 @@
+local node = self.node
+local cmd = self.cmd
+
+local function animation(self)
+  local listener_id = nil
+  local orig_pos = node:get_pos()
+  local wid = 0
+  local cid = 0
+  local idx = 0
+
+  local function animation_listener()
+    cid = cmd:move({ after=cid, time=0, relative=false }, vec3(2, 0, -1))
+    cid = cmd:move({ after=cid, time=10, relative=true }, vec3(-4, 0, 0))
+    cid = cmd:move({ after=cid, time=10, relative=true }, vec3(4, 0, 0))
+
+    cid = cmd:emit(
+      { after=cid },
+      { type=Event.SCRIPT_RESUME, listener=listener_id})
+  end
+
+  listener_id = self:listen(animation_listener, {Event.SCRIPT_RESUME})
+
+  cmd:emit(
+    {},
+    { type=Event.SCRIPT_RESUME, listener=listener_id})
+end
+
+animation(self)
