33cd7f260ef66a0e7266dafee48c51bde50c2b39
Kari Ikonen <mr.kari.ikonen@gmail.com>
2026-01-04 18:15:25 +0200

    FIX batch was failing due to drawableIndex off by one

--- src/render/BatchRegistry.cpp
+++ src/render/BatchRegistry.cpp
@@ -7,24 +7,24 @@ namespace
 }
 
 namespace render {
-    int16_t BatchRegistry::getMultiDrawIndex(const MultiDrawKey& multiDraw)
+    uint16_t BatchRegistry::getMultiDrawIndex(const MultiDrawKey& multiDraw)
     {
         const auto& it = m_multiDrawIndeces.find(multiDraw);
         if (it != m_multiDrawIndeces.end()) return it->second;
 
-        auto index = static_cast<int16_t>(m_multiDraws.size());
+        auto index = static_cast<uint16_t>(m_multiDraws.size());
         m_multiDraws.push_back(multiDraw);
         m_multiDrawIndeces.insert({ multiDraw, index });
 
         return index;
     }
 
-    int16_t BatchRegistry::getCommandIndex(const CommandKey& cmd)
+    uint16_t BatchRegistry::getCommandIndex(const CommandKey& cmd)
     {
         const auto& it = m_commandIndeces.find(cmd);
         if (it != m_commandIndeces.end()) return it->second;
 
-        auto index = static_cast<int16_t>(m_commands.size());
+        auto index = static_cast<uint16_t>(m_commands.size());
         m_commands.push_back(cmd);
         m_commandIndeces.insert({ cmd, index });
 
