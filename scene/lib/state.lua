785f51193c05bf974af60fe6814309927ca1dcbe
Kari Ikonen <mr.kari.ikonen@gmail.com>
2026-05-02 22:38:31 +0300

    FIX new "Base state" logic

--- scene/lib/state.lua
+++ scene/lib/state.lua
@@ -1,27 +1,31 @@
-local State = {}
+local BaseState = {}
 
-function State:new_class(o)
+-- New node type specific state class
+-- @param o { type_id: }
+function BaseState:new_class(o)
   o = o or {}
   setmetatable(o, self)
   self.__index = self
   return o
 end
 
-function State:new(o)
+-- new state instance
+function BaseState:new(o)
   o = o or {}
   setmetatable(o, self)
   self.__index = self
   return o
 end
 
-function State:_init(o)
+-- one time init for State instance
+function BaseState:_init()
   if not self._initialized and self.initialize then
     self:initialize()
     self._initialized = true
   end
 end
 
-function State:destroy(o)
+function BaseState:destroy()
   debug("destroy state: %s\n", self.handle)
 
   states[self.handle] = nil
@@ -37,17 +41,18 @@ function State:destroy(o)
   end
 end
 
-function State:class()
+-- get class for state
+function BaseState:class()
   return getmetatable(self)
 end
 
-function State:add_updater(fn)
+function BaseState:add_updater(fn)
   -- debug("STATE: %s: Add updater: %s\n", self.handle, fn)
   self.updaters = self.updaters or {}
   self.updaters[#self.updaters + 1] = fn
 end
 
-function State:listen(fn, types)
+function BaseState:listen(fn, types)
   listener_id = events:listen(fn, types)
 
   debug("STATE: %s: listen: listener=%d, fn=%s, types={%s}\n", self.handle, listener_id, fn, table_format(types))
@@ -58,21 +63,21 @@ function State:listen(fn, types)
   return listener_id
 end
 
--- function State:ditto()
---   print("DITTO")
--- end
-
--- function State:start()
--- end
-
--- function State:update(dt)
--- end
-
-function state_create(Actor, o)
-  o = o or {}
-  setmetatable(o, Actor)
-  Actor.__index = Actor
-  return o
+local function _create_state(id, typeId)
+  states[id] = classes[typeId]:new()
 end
 
-return State
+local function _destroy_state(id)
+  print("-------")
+  print(states)
+  print("-------")
+  if states[id] then
+    states[id]:destroy()
+    states[id] = nil
+  end
+end
+
+_G._create_state = _create_state
+_G._destroy_state = _destroy_state
+
+return BaseState
