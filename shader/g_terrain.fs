75786b7f7e032c981e161d1a82e516ddcfe6a85f
Kari Ikonen <kari.ikonen@freska.fi>
2026-07-03 16:21:15 +0300

    FIX renamed

--- shader/g_terrain.fs
+++ shader/g_terrain.fs
@@ -7,7 +7,7 @@
 #include "include/uniform_data.glsl"
 #include "include/uniform_debug.glsl"
 
-#include "include/water_caustics.glsl"
+#include "include/fn_water_caustics.glsl"
 
 // https://www.khronos.org/opengl/wiki/Early_Fragment_Test
 // https://www.gamedev.net/forums/topic/700517-performance-question-alpha-texture-vs-frag-shader-discard/5397906/
